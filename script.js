da201f28aaf693ab75dbb52c16986804906cb5b6
Génesis Reyes <114014529+Genesisreyes18@users.noreply.github.com>
2025-03-09 13:36:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function showPage(pageId) {
     });
     document.getElementById(pageId).classList.remove('hidden');
 }
+//COMIC PAGE
+let currentIndex = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function changeSlide(step) {
+    slides[currentIndex].classList.remove("active");
+    dots[currentIndex].classList.remove("active");
+
+    currentIndex = (currentIndex + step + slides.length) % slides.length;
+
+    slides[currentIndex].classList.add("active");
+    dots[currentIndex].classList.add("active");
+}
+
+function goToSlide(index) {
+    slides[currentIndex].classList.remove("active");
+    dots[currentIndex].classList.remove("active");
+
+    currentIndex = index;
+
+    slides[currentIndex].classList.add("active");
+    dots[currentIndex].classList.add("active");
+}
+
